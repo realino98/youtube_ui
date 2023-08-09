f2ba05bc575653c03ffd705ffad61fad6c63d0a3
Bernard <bernard.realino98@gmail.com>
2023-08-09 11:34:12 +0700

    BR : Modify Views Count

--- lib/models/video_model.dart
+++ lib/models/video_model.dart
@@ -25,7 +25,29 @@ class Video {
       thumbnail: video['snippet']['thumbnails']['medium']['url'],
       channelTitle: video['snippet']['channelTitle'],
       published: video['snippet']['publishedAt'],
-      views: video['statistics']['viewCount'],
+      views: viewCount(video['statistics']['viewCount']),
     );
   }
+}
+
+String viewCount(String views) {
+  String _tails = "";
+  num viewCount = num.parse(views);
+
+  if (viewCount <= 1000) {
+    viewCount = viewCount;
+  } else if (viewCount <= 1000000) {
+    viewCount = viewCount ~/ 1000;
+    _tails = "K";
+  } else if (viewCount <= 1000000000) {
+    viewCount = viewCount ~/ 1000000;
+    _tails = "M";
+  } else if (viewCount <= 1000000000000) {
+    viewCount = viewCount ~/ 1000000000;
+    _tails = "B";
+  } else {
+    viewCount = viewCount ~/ 1000000000;
+    _tails = "B";
+  }
+  return "${viewCount}${_tails}";
 }
