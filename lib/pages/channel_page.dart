4dded43f08bcb1f97cec35dfd6185b0de40cc55f
Bernard <bernard.realino98@gmail.com>
2023-08-22 10:17:45 +0700

    BR : Added Loading in Channel Video

--- lib/pages/channel_page.dart
+++ lib/pages/channel_page.dart
@@ -37,113 +37,122 @@ class _ChannelPageState extends State<ChannelPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 100,
-                child: Center(
-                  child: IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    onPressed: () {},
+      body: Visibility(
+        visible: !_isLoading,
+        replacement: Container(
+          child: Center(
+              child: CircularProgressIndicator(
+            color: Colors.grey,
+          )),
+        ),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: Center(
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () {},
+                    ),
                   ),
                 ),
-              ),
-              Visibility(
-                visible: !_isLoading,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 210,
-                      width: 1700,
-                      color: Colors.white,
-                      // child: Image.network(
-                      //   _channel.profilePicture,
-                      //   fit: BoxFit.cover,
-                      // ),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Container(
-                      width: 1600,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: CircleAvatar(
-                                  radius: 25.0,
-                                  backgroundImage:
-                                      NetworkImage(_channel.profilePicture),
-                                  backgroundColor: Colors.transparent,
-                                ),
-                              ),
-                              Column(
-                                children: [
-                                  Text(
-                                    _channel.title,
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  Text("${_channel.subscribers} Subscribers"),
-                                ],
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text("Watch Preview"),
-                              ),
-                              SizedBox(
-                                width: 20,
-                              ),
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text("Subscribe"),
-                              ),
-                            ],
-                          ),
-                        ],
+                Visibility(
+                  visible: !_isLoading,
+                  replacement: Container(
+                    child: const Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.grey,
                       ),
                     ),
-                    Container(
-                      height: 500,
-                      width: 1600,
-                      child: !_isLoading
-                          ? ListView.builder(
-                              itemCount: _channel.channelVideos.length,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: (context, index) {
-                                return Padding(
-                                  padding: const EdgeInsets.only(right: 10.0),
-                                  child: VideoThumbnail(
-                                      video: _channel.channelVideos[index]),
-                                );
-                              },
-                            )
-                          : CircularProgressIndicator(),
-                    )
-                  ],
-                ),
-                replacement: Container(
-                  child: Center(
-                    child: CircularProgressIndicator(
-                      color: Colors.grey,
-                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 210,
+                        width: 1700,
+                        color: Colors.white,
+                        // child: Image.network(
+                        //   _channel.profilePicture,
+                        //   fit: BoxFit.cover,
+                        // ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      SizedBox(
+                        width: 1600,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: CircleAvatar(
+                                    radius: 25.0,
+                                    backgroundImage:
+                                        NetworkImage(_channel.profilePicture),
+                                    backgroundColor: Colors.transparent,
+                                  ),
+                                ),
+                                Column(
+                                  children: [
+                                    Text(
+                                      _channel.title,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text("${_channel.subscribers} Subscribers"),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text("Watch Preview"),
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text("Subscribe"),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 500,
+                        width: 1600,
+                        child: !_isLoading
+                            ? ListView.builder(
+                                itemCount: _channel.channelVideos.length,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(right: 10.0),
+                                    child: VideoThumbnail(
+                                        video: _channel.channelVideos[index]),
+                                  );
+                                },
+                              )
+                            : CircularProgressIndicator(),
+                      )
+                    ],
                   ),
                 ),
-              ),
-            ],
-          );
-        },
+              ],
+            );
+          },
+        ),
       ),
     );
   }
